1dc1ed10d70673222eb9977c14ca600823dc9aa7
Megan Day <meganeday7@gmail.com>
2021-08-11 14:54:31 -0500

    translated to react app

---
+++ portfolio/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Home from './components/Home';
+import ProjectPage from './components/ProjectPage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Home /> */}
+      <ProjectPage />
+    </div>
+  );
+}
+
+export default App;
